493b3c38eb5bc1c77a29868c5c719671f99bb25c
Eddie Shin <eshinhw@gmail.com>
2023-02-09 16:28:32 -0500

    working on smart contract of Dappazon

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,50 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+/*
+- Smart Contract is a backend of the website. 
+- Smart Contract is written in Solidity.
+- It will be responsible for managing orders, distributing items and processing payments between the parties.
+*/
+
+contract Dappazon {
+    address public owner;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public {
+        // create Item struct
+
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        ); 
+
+        // Save Item struct to blockchain
+    }
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,34 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
-describe("Dappazon", () => {
+// Writing tests is extremely important for Smart Contract development!!
 
-})
+describe("Dappazon Testing", () => {
+  let dappazon;
+  let deployer, buyer;
+
+  beforeEach(async () => {
+    // Set up fake accounts
+    // ehthers.getSigners() returns a set of fake accounts created by ethers
+    [deployer, buyer] = await ethers.getSigners();
+    // console.log(await ethers.getSigners());
+    // console.log(deployer, buyer)
+    console.log(deployer.address)
+
+    // Deploy contract
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+  });
+  describe("Deployment", () => {
+    it("sets the owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    });
+    it("has a name", async () => {
+      expect(await dappazon.name()).to.equal("Dappazon");
+    });
+  });
+});
