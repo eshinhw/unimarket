e7a28ed886409ed5761cd95b4433acd103dbae9d
Eddie Shin <eshinhw@gmail.com>
2023-02-24 00:00:40 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import Section from "./components/Section";
 import Product from "./components/Product";
 
 // ABIs
-import Dappsla from "./abis/Dappazon.json";
+import DappslaABI from "./abis/Dappsla.json";
 
 // Config
 import config from "./config.json";
 // import Navbar from "./components/BasicExample";
 import BasicExample from "./components/Navigation";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [account, setAccount] = useState(null);
   const [provider, setProvider] = useState(null);
+  const [dappsla, setDappsla] = useState(null);
+  const [vehicles, setVehicles] = useState(null);
+  const [charging, setCharging] = useState(null);
 
   const loadBlockchainData = async () => {
     const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
@@ -29,10 +32,33 @@ function App() {
     // Same logic, ethers.js turns normal app into blockchain app
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     setProvider(provider);
-    console.log("hello?")
-    const network = await provider.getNetwork()
-    console.log(network)
+    const network = await provider.getNetwork();
+    console.log(network);
     // providing connection to blockchain inside the app
+    // ABI: Abstract Binary Interface
+    const dappsla = new ethers.Contract(
+      config[network.chainId].dappsla.address,
+      DappslaABI,
+      provider
+    );
+
+    setDappsla(dappsla);
+    // 2. Connect to smart contract
+    // 3. Load products
+
+    console.log(dappsla);
+    const cars = [];
+    for (let i = 0; i < 9; i++) {
+      const car = await dappsla.cars(i + 1);
+      cars.push(car);
+    }
+
+    console.log(cars);
+    console.log("helo");
+
+    const vehicles = cars.filter((car) => car.model_class === "Sedan" || car.model_class === "SUV");
+    console.log(vehicles);
+    setVehicles(vehicles);
     // 2. Connect to smart contract
     // 3. Load products
   };
@@ -42,11 +68,14 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <Navigation />
-      <h2>Welcome to Dappsla!</h2>
-      <p>{account}</p>
-    </div>
+    <Routes>
+
+
+    </Routes>
+    // <div>
+    //   <Navigation />
+    //   {vehicles && <Section title={"Electric Cars"} cars={vehicles} />}
+    // </div>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,43 +1,35 @@
 import { ethers } from "ethers";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 
 const Navigation = ({ account, setAccount }) => {
   const connectHandler = () => {
     console.log("connecting...");
   };
   return (
-    <nav className="nav">
-      <div className="nav__brand">
-        <h1>DAPPSLA</h1>
-      </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Nav.Link href="/" style={{fontSize: "50px"}}>DAAPSLA</Nav.Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Nav style={{ marginRight: "15px" }} className="justify-content-center">
+          <Nav.Link>ELECTRIC CARS</Nav.Link>
+          <Nav.Link>ELECTRIC GADGETS</Nav.Link>
+          <Nav.Link>BOOKS & MAGAZINES</Nav.Link>
+          <Nav.Link>CLOTHING</Nav.Link>
+        </Nav>
+        <Button>CONNECT</Button>
+      </Container>
+    </Navbar>
 
-      <ul className="nav__links">
-          <li>
-            <a href="#Clothing & Jewelry">Model S</a>
-          </li>
-          <li>
-            <a href="#Clothing & Jewelry">Model 3</a>
-          </li>
-          <li>
-            <a href="#Clothing & Jewelry">Model X</a>
-          </li>
-          <li>
-            <a href="#Clothing & Jewelry">Model Y</a>
-          </li>
-          <li>
-            <a href="#Clothing & Jewelry">Cybertruck</a>
-          </li>
-        </ul>
-
-      {account ? (
-        <button type="button" className="nav__connect">
-          {account.slice(0, 5) + "..." + account.slice(38, 42)}
-        </button>
-      ) : (
-        <button type="button" className="nav__connect" onClick={connectHandler}>
-          Connect to MetaMask
-        </button>
-      )}
-    </nav>
+    //   {account ? (
+    //     <button type="button" className="nav__connect">
+    //       {account.slice(0, 5) + "..." + account.slice(38, 42)}
+    //     </button>
+    //   ) : (
+    //     <button type="button" className="nav__connect" onClick={connectHandler}>
+    //       Connect to MetaMask
+    //     </button>
+    //   )}
+    // </nav>
   );
 };
 
