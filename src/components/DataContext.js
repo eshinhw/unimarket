da18d33a01dbfdfc818fb9f98497e1c923bff1de
Eddie Shin <eshinhw@gmail.com>
2023-02-24 12:15:21 -0500

    removed DataContext.js

--- src/components/DataContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const DataContext = (props) => {
-  const itemContext = createContext(null);
-  value = {}
-  return (  );
-}
- 
-export default DataContext;
