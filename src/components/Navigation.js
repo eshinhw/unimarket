3782692dcfa12382c5998fc5d858e5ade95583ed
Eddie Shin <eshinhw@gmail.com>
2023-02-24 12:14:59 -0500

    updated upto displying different categories

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,52 @@
 import { ethers } from "ethers";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 
-const Navigation = ({ account, setAccount }) => {
+const Navigation = ({ props }) => {
   const connectHandler = () => {
     console.log("connecting...");
   };
+
+  const handleOnClick = (e) => {
+    const currText = e.target.innerText;
+
+    if (currText === "ELECTRIC CARS") {
+      props[0](true);
+      props[1](false);
+      props[2](false);
+      props[3](false);
+    }
+    if (currText === "PERSONAL GADGETS") {
+      props[0](false);
+      props[1](true);
+      props[2](false);
+      props[3](false);
+    }
+    if (currText === "BOOKS & MAGAZINES") {
+      props[0](false);
+      props[1](false);
+      props[2](true);
+      props[3](false);
+    }
+    if (currText === "CLOTHING") {
+      props[0](false);
+      props[1](false);
+      props[2](false);
+      props[3](true);
+    }
+  };
+
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Nav.Link href="/" style={{fontSize: "50px"}}>UniMarket</Nav.Link>
+        <Nav.Link href="/" style={{ fontSize: "50px" }}>
+          UniMarket
+        </Nav.Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Nav style={{ marginRight: "15px" }} className="justify-content-center">
-          <Nav.Link>ELECTRIC CARS</Nav.Link>
-          <Nav.Link>PERSONAL GADGETS</Nav.Link>
-          <Nav.Link>BOOKS & MAGAZINES</Nav.Link>
-          <Nav.Link>CLOTHING</Nav.Link>
+          <Nav.Link onClick={(e) => handleOnClick(e)}>ELECTRIC CARS</Nav.Link>
+          <Nav.Link onClick={(e) => handleOnClick(e)}>PERSONAL GADGETS</Nav.Link>
+          <Nav.Link onClick={(e) => handleOnClick(e)}>BOOKS & MAGAZINES</Nav.Link>
+          <Nav.Link onClick={(e) => handleOnClick(e)}>CLOTHING</Nav.Link>
         </Nav>
         <Button>CONNECT TO WALLET</Button>
       </Container>
