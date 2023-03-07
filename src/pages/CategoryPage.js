b3e98b1ed97f846f758fb4b9704adbfe9b59fb3b
Eddie Shin <eshinhw@gmail.com>
2023-03-06 19:44:28 -0500

    fixed styles for CategoryPage

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -2,6 +2,7 @@ import { prettyFormat } from "@testing-library/react";
 import React, { useEffect, useState } from "react";
 import Product from "../components/Product";
 import sampleData from "../testData.json";
+import "../css/CategoryPage.css"
 
 function CategoryPage({ category }) {
   const [products, setProducts] = useState(null);
@@ -19,24 +20,30 @@ function CategoryPage({ category }) {
     loadProducts();
   }, []);
 
-  return loaded ? (
-    <div className="home__row">
-      {products.map((item, idx) => (
-        <Product
-          className="home__row__item"
-          id={item.id}
-          title={item.name}
-          category={item.category}
-          image={item.image}
-          price={item.price}
-          rating={item.rating}
-          inventory={item.inventory}
-          key={idx}
-        />
-      ))}
+  return (
+    <div className="category__page">
+      <p>{category}</p>
+      <br />
+      {loaded ? (
+        <div className="home__row">
+          {products.map((item, idx) => (
+            <Product
+              className="home__row__item"
+              id={item.id}
+              title={item.name}
+              category={item.category}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+              inventory={item.inventory}
+              key={idx}
+            />
+          ))}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
-  ) : (
-    <></>
   );
 }
 
