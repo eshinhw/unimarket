e241b3c3a22b8042b6dd97884d620f7d43878ed6
Eddie Shin <eshinhw@gmail.com>
2023-03-06 19:32:19 -0500

    hold on reducer... not working for now

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,8 +3,19 @@ import "../css/Product.css";
 import starSolid from "../assets/star-solid.svg";
 import starRegular from "../assets/star-regular.svg";
 import { Button } from "react-bootstrap";
+import { useStateValue } from "../StateProvider";
 
 function Product({ title, price, image, rating }) {
+  // { basket }: state of global store
+  // dispatch: how we manipulate data layer
+  // const [{ basket }, dispatch] = useStateValue();
+  const addToCart = () => {
+    // dispatch the item into the data layer
+    // dispatch({
+    //   type: "ADD_TO_BASKET",
+    //   item: { title: title, price: price, image: image, rating: rating },
+    // });
+  };
   return (
     <div className="product">
       <div className="product__info">
@@ -23,7 +34,7 @@ function Product({ title, price, image, rating }) {
       </div>
       <img src={image} />
       <div className="product__buttons">
-        <Button>Add to Cart</Button>
+        <Button onClick={addToCart}>Add to Cart</Button>
         <Button>Order</Button>
       </div>
     </div>
