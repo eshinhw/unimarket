019ebc3efdb6248e0033291bdeb646312ec98ed7
Eddie Shin <eshinhw@gmail.com>
2023-03-06 19:31:32 -0500

    implemented CategoryPage

---
+++ src/pages/CategoryPage.js
@@ -0,0 +1,43 @@
+import { prettyFormat } from "@testing-library/react";
+import React, { useEffect, useState } from "react";
+import Product from "../components/Product";
+import sampleData from "../testData.json";
+
+function CategoryPage({ category }) {
+  const [products, setProducts] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  console.log(category);
+  const loadProducts = async () => {
+    const products = await sampleData.data.filter((item) => item.category === category);
+    console.log(products);
+    setProducts(products);
+    setLoaded(true);
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  return loaded ? (
+    <div className="home__row">
+      {products.map((item, idx) => (
+        <Product
+          className="home__row__item"
+          id={item.id}
+          title={item.name}
+          category={item.category}
+          image={item.image}
+          price={item.price}
+          rating={item.rating}
+          inventory={item.inventory}
+          key={idx}
+        />
+      ))}
+    </div>
+  ) : (
+    <></>
+  );
+}
+
+export default CategoryPage;
